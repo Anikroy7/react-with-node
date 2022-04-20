f0de8b476da6df601813dd7dec51d2c29ab2ec32
Anikroy7 <roy645800@gmail.com>
2022-04-20 19:13:57 +0600

    post method apply for set data in bakend

--- src/App.js
+++ src/App.js
@@ -13,9 +13,32 @@ function App() {
   }, [])
 
 
+  const handleFormSubmit = event => {
+    event.preventDefault()
+    const name = event.target.name.value;
+    const hobby = event.target.hobby.value;
+    const user = { name, hobby }
+    console.log(user);
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+
+
   return (
     <div className="App">
       <h1>our own data</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name="name" id="1" />
+        <input type="text" name="hobby" id="1" />
+        <input type="submit" value="Submit" />
+      </form>
       {
         users.map(user => <li key={user.id}>Name: {user.name} hobby: {user.hobby} age:{user.age}</li>)
       }
