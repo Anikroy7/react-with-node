d3e39f742682e40e4b4271e8bb10838cb59e0bc1
Anikroy7 <roy645800@gmail.com>
2022-04-20 18:30:29 +0600

    shared data by from port by using cors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+
   return (
     <div className="App">
       <h1>our own data</h1>
+      {
+        users.map(user => <li key={user.id}>Name: {user.name} hobby: {user.hobby} age:{user.age}</li>)
+      }
     </div>
   );
 }
